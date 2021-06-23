2bafdc9f969287835629ee1bb731ea4b8f946c7c
polat <aydinpolat@post.com>
2021-06-23 16:39:57 +0300

    Pagination is added to project.

--- app/src/main/java/com/example/news/pagination/PaginationSource.kt
+++ app/src/main/java/com/example/news/pagination/PaginationSource.kt
@@ -3,22 +3,25 @@ package com.example.news.pagination
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.news.model.Article
-import com.example.news.service.ApiService
 import com.example.news.service.Request
 
 class PaginationSource : PagingSource<Int, Article>() {
     override fun getRefreshKey(state: PagingState<Int, Article>) = 1
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
-        val page = params.key ?: 1
-        val pageSize = params.loadSize ?: 1
-        val prevKey = if (page == 1) null else page - 1
+        return try {
+            val page = params.key ?: 1
+            val pageSize = params.loadSize ?: 1
+            val prevKey = if (page == 1) null else page - 1
+            val response = Request.getNews2(page, pageSize)
+            LoadResult.Page(
+                data = response.articles,
+                prevKey = prevKey,
+                nextKey = page.plus(1)
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
 
-        val response = Request.getNews2(page, pageSize)
-        return LoadResult.Page(
-            data = response.articles,
-            prevKey = prevKey,
-            nextKey = page.plus(1)
-        )
     }
 }
